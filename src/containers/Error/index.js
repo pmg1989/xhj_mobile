10cd0168df321d725e4bea6eb75250e1be1cfa5b
pmg1989 <972401854@qq.com>
2018-04-04 22:51:48 +0800

    fix Error

--- src/containers/Error/index.js
+++ src/containers/Error/index.js
@@ -5,11 +5,11 @@ import styles from './Error.less'
 const Error = () => {
   return (
     <div className="content-box">
-      <Header>页面找不到了</Header>
+      <Header>页面未找到</Header>
       <div className="content">
         <div className={styles.box}>
           <Icon className={styles.icon} type={require('svg/cry.svg')} />
-          <p>抱歉，您访问的页面找不到了</p>
+          <p>抱歉哦~您访问的页面已经逃离地球表面</p>
         </div>
       </div>
     </div>
