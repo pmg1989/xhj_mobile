ac5eb21140a1a3496c7f1bd9670d4c33158f4438
pmg1989 <972401854@qq.com>
2018-04-08 16:19:17 +0800

    fix TabBar

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,22 +1,3 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-const Container = ({ renderHeader, renderTabBar, children }) => {
-  return (
-    <div className="content-box">
-      {renderHeader}
-      <div className="content">
-        {children}
-      </div>
-      {renderTabBar}
-    </div>
-  )
-}
-
-Container.propTypes = {
-  renderHeader: PropTypes.node,
-  renderTabBar: PropTypes.node,
-  children: PropTypes.node,
-}
+import Container from './Container'
 
 export default Container
