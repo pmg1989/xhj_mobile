d14af4eaabf75fafdb31295a8ed4fc303e87c77c
pmg1989 <972401854@qq.com>
2018-04-09 17:28:04 +0800

    fix help/GridHeader

--- src/containers/Help/Index/GridHeader.js
+++ src/containers/Help/Index/GridHeader.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 import { Grid } from 'antd-mobile'
 import styles from './GridHeader.less'
 
@@ -10,8 +11,26 @@ const GridHeader = () => {
     { icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png', text: '投诉建议' },
   ]
 
+  const links = [
+    '/help/howtouse',
+    '/help/howtoorder',
+    '/help/howtomaintain',
+    '/help/complaintadvice',
+  ]
+
   return (
-    <Grid className={styles.thumb_box} data={data} square={false} hasLine={false} />
+    <Grid
+      className={styles.thumb_box}
+      data={data}
+      square={false}
+      hasLine={false}
+      renderItem={(item, index) => (
+        <Link className={styles.item_content} to={links[index]}>
+          <img className={styles.grid_icon} src={item.icon} alt="" />
+          <div className={styles.grid_text}>{item.text}</div>
+        </Link>
+      )}
+    />
   )
 }
 
